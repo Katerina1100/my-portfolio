9404786a925188dd39b8581957736d81490bedc1
Katerina <trajkovskakaterina0@gmail.com>
2025-03-05 04:49:53 +0100

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React, { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App.jsx';
 
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
